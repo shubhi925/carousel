2be0bc86dc5cc6b0e28635de7846faeb03348186
shubhi925 <shubhichakrawarti9@gmail.com>
2025-03-05 12:45:03 +0530

    modified the functionalities

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
-const pages = document.querySelectorAll(".page");
-const nextBtns = document.querySelectorAll(".nextBtn");
-const prevBtns = document.querySelectorAll(".prevBtn");
-const btn = document.querySelectorAll(".btn");
+const carouselImg = document.querySelector(".carouselImage");
+const nextBtn = document.querySelector(".nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+const btns = document.querySelectorAll(".btn");
 
-nextBtns.forEach((nextBtn, index) => {
-  nextBtn.addEventListener("click", () => nextPage(index));
+nextBtn.addEventListener("click", () => nextImg());
+prevBtn.addEventListener("click", () => prevPage());
+
+btns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    currentImg = index;
+    updateCarousel();
+  });
 });
 
-prevBtns.forEach((prevBtn, index) => {
-  prevBtn.addEventListener("click", () => prevPage(index));
-});
+let currentImg = 0;
+setInterval(changeImg, 3000);
 
-function nextPage(index) {
-  if (index < 3) {
-    pages[index].classList.add("hidden");
-    pages[index + 1].classList.remove("hidden");
-    btn[index + 1].classList.add("bg-lime-600");
+function changeImg() {
+  currentImg++;
+  if (currentImg > 3) {
+    currentImg = 0;
   }
+  updateCarousel();
 }
-function prevPage(index) {
-  if (index > 0) {
-    pages[index].classList.add("hidden");
-    pages[index - 1].classList.remove("hidden");
-    btn[index].classList.remove("bg-lime-600");
+
+function nextImg() {
+  currentImg++;
+  if (currentImg > 3) {
+    currentImg = 0;
   }
+  updateCarousel();
+}
+
+function prevPage() {
+  currentImg--;
+  if (currentImg < 0) {
+    currentImg = 3;
+  }
+  updateCarousel();
+}
+
+function updateCarousel() {
+  carouselImg.src = `./images/img_${currentImg}.jpg`;
+  btns.forEach((btn, index) => {
+    if (index == currentImg) {
+      btn.classList.add("bg-gray-400");
+      btn.classList.remove("bg-gray-700");
+    } else {
+      btn.classList.add("bg-gray-700");
+      btn.classList.remove("bg-gray-400");
+    }
+  });
 }
